ad3b021b10b421d74ce4c8572ea4fbacbaa0ebd3
Nikola Gerdzhikov <gerdjikov17@abv.bg>
2018-04-16 19:02:11 +0300

    Add files via upload

---
+++ CriteriaPattern/src/CriteriaSingle.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CriteriaSingle implements Criteria{
+	@Override
+	public List<Person> meetCriteria(List<Person> persons) {
+		List<Person> singlePersons = new ArrayList<Person>();
+		for(Person person : persons) {
+			if(person.getMaritalStatus().equalsIgnoreCase("Single")) singlePersons.add(person);
+		}
+		return singlePersons;
+	}
+}
